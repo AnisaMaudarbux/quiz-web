5f48a304544788caf242ccc6039df59d42f3fc50
Admin <you@example.com>
2023-02-21 15:07:44 +0000

    changed back-end URL

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quizapiam-env-2.eba-cbt7kafr.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
