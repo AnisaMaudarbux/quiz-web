cdb1806cca3512d16eb02e7e0a7ce439678dc965
Admin <you@example.com>
2023-02-22 10:13:10 +0000

    changed back-end URL

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'https://quizapiam-env-2.eba-cbt7kafr.eu-west-2.elasticbeanstalk.com/';
+    service.apiBase = 'https://quizapiam-env.eba-cbt7kafr.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
